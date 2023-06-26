926b14cfe28288b48e7625b153321b62f5181b3c
hollowdll <pixerchat@gmail.com>
2023-06-26 20:08:56 +0300

    serialize and deserialize

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,16 @@
-// Include generated Rust code which is generated from employees.proto
+use std::io::Cursor;
+use prost::{
+    Message,
+    DecodeError,
+};
+
+// Include generated Rust code which is generated from employees.proto.
 pub mod employees {
     include!(concat!(env!("OUT_DIR"), "/employees.rs"));
 }
 
-/// Creates a new employee based on the Employee in employees.proto
-pub fn create_employee() -> employees::Employee {
+/// Creates a new employee based on the Employee in employees.proto.
+pub fn create_employee_populated() -> employees::Employee {
     let mut employee = employees::Employee::default();
     employee.id = 1;
     employee.email = "john@smith.com".to_string();
@@ -12,4 +18,22 @@ pub fn create_employee() -> employees::Employee {
     employee.set_work_type(employees::employee::WorkType::Hybrid);
 
     return employee
+}
+
+pub fn create_employee() -> employees::Employee {
+    employees::Employee::default()    
+}
+
+/// Serializes employee and encodes it to a buffer (binary format).
+pub fn serialize_employee(employee: &employees::Employee) -> Vec<u8> {
+    let mut buf = Vec::new();
+    buf.reserve(employee.encoded_len());
+    employee.encode(&mut buf).unwrap();
+
+    return buf
+}
+
+/// Deserialize employee buffer from binary format to rust data structure.
+pub fn deserialize_employee(buf: &[u8]) -> Result<employees::Employee, DecodeError> {
+    employees::Employee::decode(&mut Cursor::new(buf))
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use protobuf_rust::create_employee;
+use protobuf_rust::create_employee_populated;
 
 fn main() {
-    let employee = create_employee();
+    let employee = create_employee_populated();
     println!("{:?}", employee);
 }

86ad8026127daee195f65deebc76d7fe6433c2a8
hollowdll <pixerchat@gmail.com>
2023-06-26 21:39:33 +0300

    file write and read

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,19 @@
-use std::io::Cursor;
+use std::{
+    io::{
+        Cursor,
+        Write,
+    },
+    fs::{
+        self,
+        File,
+    }
+};
 use prost::{
     Message,
     DecodeError,
 };
+
+const FILE_NAME: &str = "employee_buffer.txt";
 
 // Include generated Rust code which is generated from employees.proto.
 pub mod employees {
@@ -20,6 +31,7 @@ pub fn create_employee_populated() -> employees::Employee {
     return employee
 }
 
+/// Creates a new employee with default values.
 pub fn create_employee() -> employees::Employee {
     employees::Employee::default()    
 }
@@ -33,7 +45,21 @@ pub fn serialize_employee(employee: &employees::Employee) -> Vec<u8> {
     return buf
 }
 
-/// Deserialize employee buffer from binary format to rust data structure.
+/// Deserializes employee buffer from binary format to rust data structure.
 pub fn deserialize_employee(buf: &[u8]) -> Result<employees::Employee, DecodeError> {
     employees::Employee::decode(&mut Cursor::new(buf))
+}
+
+/// Writes employee as buffer to a file.
+pub fn write_employee_to_file(buf: &[u8]) {
+    let mut file = File::create(FILE_NAME).unwrap();
+    file.write(buf).unwrap();
+}
+
+/// Reads employee as buffer from a file.
+pub fn read_employee_from_file() -> employees::Employee {
+    let buf = fs::read(FILE_NAME).unwrap();
+    let employee = deserialize_employee(&buf).unwrap();
+
+    return employee
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
-use protobuf_rust::create_employee_populated;
+use protobuf_rust::*;
 
 fn main() {
-    let employee = create_employee_populated();
+    let mut employee = create_employee_populated();
     println!("{:?}", employee);
+    employee.age = 50;
+
+    write_employee_to_file(&serialize_employee(&employee));
+    let employee_read = read_employee_from_file();
+    println!("{:?}", employee_read);
 }
